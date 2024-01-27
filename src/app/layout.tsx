27a100d32bbb461e9d2801f659ce29a206fb89db
Seun Oyediran <seunoyediran22517@gmail.com>
2024-01-27 18:52:17 +0100

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,6 @@ import type { Metadata } from "next";
 
 import "../../public/style/global.css";
 
-export enum Controls {
-  forward = "forward",
-  back = "back",
-  left = "left",
-  right = "right",
-  jump = "jump",
-  run = "run",
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
